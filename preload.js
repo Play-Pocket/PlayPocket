a4bc8f50e3262c6f0b4d40027bca5022aee64ce8
TakkunMC_jp <Kaneda.family.803@gmail.com>
2026-04-02 15:57:56 +0900

    Update preload.js

--- preload.js
+++ preload.js
@@ -4,3 +4,7 @@ contextBridge.exposeInMainWorld('api', {
   readFile: (path) => ipcRenderer.invoke('read-file', path),
   writeFile: (path, data) => ipcRenderer.invoke('write-file', path, data)
 });
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  setRPC: (data) => ipcRenderer.send('set-rpc', data)
+});
