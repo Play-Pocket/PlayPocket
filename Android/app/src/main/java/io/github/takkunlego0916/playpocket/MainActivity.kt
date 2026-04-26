78705d57c7cdb535670448726fbaeba4f9e36c32
TakkunMC_jp <Kaneda.family.803@gmail.com>
2026-04-26 12:13:02 +0900

    Update MainActivity.kt

--- Android/app/src/main/java/io/github/takkunlego0916/playpocket/MainActivity.kt
+++ Android/app/src/main/java/io/github/takkunlego0916/playpocket/MainActivity.kt
@@ -1,13 +1,14 @@
 package io.github.takkunlego0916.playpocket
 
 import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var webView: WebView
+    private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,11 +16,57 @@ class MainActivity : AppCompatActivity() {
         webView = WebView(this)
         setContentView(webView)
 
-        webView.settings.javaScriptEnabled = true
-        webView.settings.domStorageEnabled = true
+        webView.settings.apply {
+            javaScriptEnabled  = true
+            domStorageEnabled  = true
 
-        webView.webViewClient = WebViewClient()
+            allowFileAccess                 = false
+            allowContentAccess              = false
+            @Suppress("DEPRECATION")
+            allowFileAccessFromFileURLs     = false
+            @Suppress("DEPRECATION")
+            allowUniversalAccessFromFileURLs = false
+
+            mixedContentMode = WebSettings.MIXED_CONTENT_NEVER_ALLOW
+
+            setSupportZoom(false)
+            builtInZoomControls  = false
+            displayZoomControls  = false
+            saveFormData         = false
+            @Suppress("DEPRECATION")
+            savePassword         = false
+        }
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                request: WebResourceRequest
+            ): Boolean {
+                val url = request.url?.toString() ?: return true
+                return when {
+                    url.startsWith("file://") -> false
+                    url.startsWith("blob:")   -> false
+                    else                      -> true
+                }
+            }
+        }
 
         webView.loadUrl("file:///android_asset/index.html")
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            @Suppress("DEPRECATION")
+            super.onBackPressed()
+        }
+    }
+
+    override fun onDestroy() {
+        webView.stopLoading()
+        webView.destroy()
+        super.onDestroy()
     }
 }
