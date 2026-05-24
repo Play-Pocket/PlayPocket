91f9d7528fde570b8fff8f477d5426b463052ebc
TakkunMC_jp <Kaneda.family.803@gmail.com>
2026-05-24 11:11:19 +0900

    Update preload.js

--- preload.js
+++ preload.js
@@ -4,6 +4,33 @@ contextBridge.exposeInMainWorld('electronAPI', {
   setRPC: (data) => ipcRenderer.send('set-rpc', data),
   clearRPC: () => ipcRenderer.send('clear-rpc'),
 
-  readFile: (path) => ipcRenderer.invoke('read-file', path),
-  writeFile: (path, data) => ipcRenderer.invoke('write-file', path, data)
+  getSettings: () => ipcRenderer.invoke('get-settings'),
+  getStartupState: () => ipcRenderer.invoke('get-startup-state'),
+  setSettings: (partial) => ipcRenderer.invoke('set-settings', partial),
+  saveRuntimeState: (partial) => ipcRenderer.invoke('save-runtime-state', partial),
+  clearBrowserCache: () => ipcRenderer.invoke('clear-browser-cache'),
+  openExternal: (url) => ipcRenderer.invoke('open-external', url),
+
+  readFile: (filePath) => {
+    if (typeof filePath !== 'string') {
+      return Promise.reject(new Error('readFile: path must be a string'));
+    }
+    return ipcRenderer.invoke('read-file', filePath);
+  },
+
+  writeFile: (filePath, data) => {
+    if (typeof filePath !== 'string') {
+      return Promise.reject(new Error('writeFile: path must be a string'));
+    }
+    if (typeof data !== 'string') {
+      return Promise.reject(new Error('writeFile: data must be a string'));
+    }
+    return ipcRenderer.invoke('write-file', filePath, data);
+  },
+
+  onPlaybackCommand: (handler) => {
+    const listener = (_, command) => handler(command);
+    ipcRenderer.on('playback-command', listener);
+    return () => ipcRenderer.removeListener('playback-command', listener);
+  }
 });
