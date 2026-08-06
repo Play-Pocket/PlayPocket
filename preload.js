c1caa6e47d30e067d363dc3e90a6db7a934a1a3a
TakkunMC_jp <Kaneda.family.803@gmail.com>
2026-08-06 16:26:45 +0900

    Update preload.js

--- preload.js
+++ preload.js
@@ -11,23 +11,6 @@ contextBridge.exposeInMainWorld('electronAPI', {
   clearBrowserCache: () => ipcRenderer.invoke('clear-browser-cache'),
   openExternal: (url) => ipcRenderer.invoke('open-external', url),
 
-  readFile: (filePath) => {
-    if (typeof filePath !== 'string') {
-      return Promise.reject(new Error('readFile: path must be a string'));
-    }
-    return ipcRenderer.invoke('read-file', filePath);
-  },
-
-  writeFile: (filePath, data) => {
-    if (typeof filePath !== 'string') {
-      return Promise.reject(new Error('writeFile: path must be a string'));
-    }
-    if (typeof data !== 'string') {
-      return Promise.reject(new Error('writeFile: data must be a string'));
-    }
-    return ipcRenderer.invoke('write-file', filePath, data);
-  },
-
   onPlaybackCommand: (handler) => {
     const listener = (_, command) => handler(command);
     ipcRenderer.on('playback-command', listener);
